691223a19f25e50e4afe6cac3dae593a5b87e78d
Jacquin Wynn <jacquinwynn@Jacquins-MacBook-Pro.local>
2016-07-02 19:50:00 -0400

    Added Category Functionality

---
+++ routes/categories.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb');
+
+
+var db = require('monk')('localhost/nodeblog');
+
+
+router.get('/add', function(req, res, next) {
+      res.render('addcategory', {
+        'title': 'Add Category'
+      });
+});
+
+router.post('/add', function(req, res, next) {
+  //Get Form VAlues
+  var name = req.body.name;
+
+
+
+  //Form VAlidation
+  req.checkBody('name', 'Name field is required').notEmpty();
+
+
+  var errors = req.validationErrors();
+
+  if (errors) {
+    res.render('addpost', {
+      "error": errors
+    });
+  } else {
+    var categories = db.get('categories');
+    categories.insert({
+      "name": name,
+    }, function(err, post){
+      if (err) {
+        res.send(err);
+      } else {
+        req.flash('success', 'Category Added');
+        res.location('/');
+        res.redirect('/');
+      }
+    });
+  }
+
+
+});
+
+module.exports = router;
