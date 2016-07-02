ae84e4038680a4598712ec0c76243c6ac658ffe1
Jacquin Wynn <jacquinwynn@Jacquins-MacBook-Pro.local>
2016-07-02 16:37:09 -0400

    Set up Middleware

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
 var moment = require('moment');
 var expressValidator = require('express-validator');
+var mongo = require('mongodb');
+
 
 var db = require('monk')('localhost/nodeblog');
 
@@ -27,6 +30,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Express Session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+//Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value){
+    var namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length){
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//Connect-Flash
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 //Makes database accessible to our router
 app.use(function(req, res, next){
